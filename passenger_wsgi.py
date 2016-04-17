79affb9ed0b8fc13ba3b79bba81edda503ca0e95
Daniel Senh Wong <danielsenhwong@gmail.com>
2016-04-17 09:31:13 -0400

    Corrected static URL for rendering template. Also moved images into static folder

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,5 @@
 #import sys
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 application = Flask(__name__)
 
 #def application(environ, start_response):
@@ -9,8 +9,11 @@ application = Flask(__name__)
 #	return [bytes(str, 'UTF-8')]
 
 @application.route("/")
-def hello():
-	return "Hello World!"
+#def hello():
+#	return "Hello World!"
+
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
 	application.run()
