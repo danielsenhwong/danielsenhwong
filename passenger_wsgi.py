7b8eaa76322070a4d2a57a6f1b8e5a9029960ef9
Daniel Senh Wong <danielsenhwong@gmail.com>
2016-04-18 13:37:02 -0400

    Completed basic database integration, initialized database. Added code from github gist to hash static files and ensure they are reloaded if changed. Source in code comment.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,6 +4,7 @@
 # More info:
 # http://kb.apisnetworks.com/cgi-passenger/passenger-application-layout/
 
+import os
 import sqlite3
 from flask import (Flask, Response, render_template, request, session, g, url_for, abort, redirect, flash)
 from contextlib import closing
@@ -47,19 +48,59 @@ def teardown_request(exception):
 #####
 # ROUTING
 #####
-# Application routes
+# From https://gist.github.com/Ostrovski/f16779933ceee3a9d181
+# Solve issue of static files not being refreshed
+@application.url_defaults
+def hashed_url_for_static_file(endpoint, values):
+    if 'static' == endpoint or endpoint.endswith('.static'):
+        filename = values.get('filename')
+        if filename:
+            if '.' in endpoint:  # has higher priority
+                blueprint = endpoint.rsplit('.', 1)[0]
+            else:
+                blueprint = request.blueprint  # can be None too
+
+            if blueprint:
+                static_folder = application.blueprints[blueprint].static_folder
+            else:
+                static_folder = application.static_folder
+
+            param_name = 'h'
+            while param_name in values:
+                param_name = '_' + param_name
+            values[param_name] = static_file_hash(os.path.join(static_folder, filename))
+            
+def static_file_hash(filename):
+  return int(os.stat(filename).st_mtime) # or app.config['last_build_timestamp'] or md5(filename) or etc...
+
+
 @application.route('/')
 def index():
-    cur = g.db.execute('select title, description from projects order by id desc')
-    projects = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    cur = g.db.execute('select title, description, url from projects order by id desc')
+    projects = [dict(title=row[0], description=row[1], url=row[2]) for row in cur.fetchall()]
     return render_template('index.html', projects=projects)
 
+@application.route('/show_projects')
 def show_projects():
-    cur = g.db.execute('select title, description from projects order by id desc')
-    projects = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('index.html', projects=projects)
+    cur = g.db.execute('select title, description, url from projects order by id desc')
+    projects = [dict(title=row[0], description=row[1], url=row[2]) for row in cur.fetchall()]
+    return render_template('show.html', projects=projects)
 
-@application.route('/add_project', methods=['GET', 'POST'])
+@application.route('/add_project', methods=['POST'])
+def add_project():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into projects (title, description, url) values (?, ?, ?)',
+            [
+                request.form['title'],
+                request.form['description'],
+                request.form['url']
+            ])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_projects'))
+
+@application.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -78,5 +119,6 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out.')
     return redirect(url_for('show_projects'))
+
 if __name__ == "__main__":
 	application.run()
