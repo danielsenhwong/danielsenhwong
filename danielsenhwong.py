1237adc2b0023fe8828e91485d9c34808cfd295f
Daniel Senh Wong <danielsenhwong@gmail.com>
2016-04-18 10:30:52 -0400

    modified database config variable name

--- danielsenhwong.py
+++ danielsenhwong.py
@@ -1,2 +1,2 @@
 project_secret = '/home/danielsenhwong/project_secrets/danielsenhwong_secret.txt'
-dev_database_cnf = '/home/danielsenhwong/project_secrets/danielsenhwong_dev_db.cnf'
+database_cnf = '/home/danielsenhwong/project_secrets/danielsenhwong_dev_db.cnf'
