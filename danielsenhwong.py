e3e978a06de601f5128251bbe3f87749a402a96b
Daniel Senh Wong <danielsenhwong@gmail.com>
2016-04-18 10:28:45 -0400

    Passenger requires application be called "application"?

--- danielsenhwong.py
+++ danielsenhwong.py
@@ -1,4 +1,2 @@
-project_secret =
-'/home/danielsenhwong/project_secrets/danielsenhwong_secret.txt'
-dev_database_cnf =
-'/home/danielsenhwong/project_secrets/danielsenhwong_dev_db.cnf'
+project_secret = '/home/danielsenhwong/project_secrets/danielsenhwong_secret.txt'
+dev_database_cnf = '/home/danielsenhwong/project_secrets/danielsenhwong_dev_db.cnf'
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,8 +5,7 @@
 # http://kb.apisnetworks.com/cgi-passenger/passenger-application-layout/
 
 import sqlite3
-from flask import Flask, Response, render_template, request, session, g,
-url_for, abort, redirect, flash
+from flask import (Flask, Response, render_template, request, session, g, url_for, abort, redirect, flash)
 from danielsenhwong import *
 
 # Tutorial/demo
@@ -25,15 +24,15 @@ from danielsenhwong import *
 #PASSWORD = 
 
 # Set up the application
-dsw = Flask(__name__)
-dsw.config.from_pyfile('/home/danielsenhwong/project_secrets/danielsenhwong_dev_db.cnf', silent=True)
+application = Flask(__name__)
+application.config.from_pyfile('danielsenhwong.py', silent=True)
 
 # Connect to the database
 def connect_db():
-    return sqlite3.connect(dsw.config['DATABASE'])
+    return sqlite3.connect(application.config['DATABASE'])
 
 # Application routes
-@dsw.route("/")
+@application.route("/")
 #def hello():
 #	return "Hello World!"
 
@@ -41,4 +40,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-	dsw.run()
+	application.run()
