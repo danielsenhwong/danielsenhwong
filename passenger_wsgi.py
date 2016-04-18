584d9c8b0e199723a94b10370f5a403c86e5ee73
Daniel Wong <me@danielsenhwong.com>
2016-04-18 10:52:41 -0400

    Added baseic database functions

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,7 +6,7 @@
 
 import sqlite3
 from flask import (Flask, Response, render_template, request, session, g, url_for, abort, redirect, flash)
-from danielsenhwong import *
+from contextlib import closing
 
 # Tutorial/demo
 #def application(environ, start_response):
@@ -25,11 +25,31 @@ from danielsenhwong import *
 
 # Set up the application
 application = Flask(__name__)
-application.config.from_pyfile('danielsenhwong.py', silent=True)
+application.config.from_pyfile('/home/danielsenhwong/project_secrets/danielsenhwong_dev_db.cnf', silent=True)
 
+# Database-related functions
 # Connect to the database
 def connect_db():
     return sqlite3.connect(application.config['DATABASE'])
+
+# Initialize the database
+def init_db():
+    with closing(connect_db()) as db:
+        with application.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+# Connect to the database before a request is made
+@application.before_request
+def before_request():
+    g.db = connect_db()
+
+# Close database connection if an exception is raised
+@application.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 # Application routes
 @application.route("/")
@@ -37,7 +57,9 @@ def connect_db():
 #	return "Hello World!"
 
 def index():
-    return render_template('index.html')
+    cur = g.db.execute('select title, description from projects order by id desc')
+    projects = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('index.html', projects=projects)
 
 if __name__ == "__main__":
 	application.run()
